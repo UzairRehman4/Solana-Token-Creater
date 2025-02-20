a65229aa7518d0162b6678ac1937e3262abf6be2
Uzair Rehman <rehmanuzair43@gmail.com>
2025-02-21 00:48:35 +0500

    code setup

--- src/contexts/ContextProvider.tsx
+++ src/contexts/ContextProvider.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
-
-const ContextProvider = () => {
-  return (
-    <div>ContextProvider</div>
-  )
-}
-
-export default ContextProvider
+import { WalletAdapterNetwork, WalletError } from "@solana/wallet-adapter-base";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider as ReactUiWalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import {
+    PhantomWalletAdapter,
+    SolflareWalletAdapter,
+    SolletWalletAdapter,
+    SolletExtensionWalletAdapter,
+    TorusWalletAdapter
+} from "@solana/wallet-adapter-wallets";
+import { Cluster, clusterApiUrl } from "@solana/web3.js";
+import { FC, ReactNode, useCallback, useMemo } from "react";
+import { AutoConnectProvider, useAutoConnect } from "./AutoConnectProvider";
+import { notify } from "../utils/notifications";
+import { NetWorkConfigurationProvider, useNetworkConfiguration } from "./NetworkConfigurationProvider";
